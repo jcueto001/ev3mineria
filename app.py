c112e082753ba6bab26ddd52a2f702f2ef4795ea
bmjcdevs <bmjcdevs@gmail.com>
2026-07-05 22:07:10 -0400

    Agregar logs y endpoint para ver datos

--- app.py
+++ app.py
@@ -47,24 +47,7 @@ def ver_datos():
         "datos": datos
     }), 200
 
-@app.route("/ver-datos", methods=["GET"])
-def ver_datos():
-    datos = []
 
-    try:
-        with open("datos_realtime.jsonl", "r", encoding="utf-8") as archivo:
-            for linea in archivo:
-                datos.append(json.loads(linea))
-    except FileNotFoundError:
-        return jsonify({
-            "mensaje": "Aún no hay datos recibidos",
-            "datos": []
-        }), 200
-
-    return jsonify({
-        "total_registros": len(datos),
-        "datos": datos
-    }), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
